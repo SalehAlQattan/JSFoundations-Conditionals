9111371ae64299ec0ed0bdadde38c3eef017ae11
Saleh AlQattan <salehalqattan@outlook.com>
2021-05-24 20:08:05 +0300

    change from if statement to trnary operator

--- calculator.js
+++ calculator.js
@@ -1,36 +1,28 @@
-const prompt = require('prompt-sync')(); // Please don't touch me :)
+const firstNumber = 3;
+const secondNumber = 5;
+const operator = '+';
 
-// The prompt method allows us to take an input from the user.
-// Run the code to see how to use prompt.
-// const name = prompt("What's your name? ");
-// console.log(`Hi, I'm ${name}.`);
-
-const firstNumber = prompt('Please Enter The First Number: ');
-const secondNumber = prompt('Please Enter The Second Number: ');
-const operator = prompt('Please Choose the operation (+, -, /, *): ');
-
-if (operator === '+') {
-  console.log(
-    `${firstNumber} + ${secondNumber} = ${
-      parseInt(firstNumber) + parseInt(secondNumber)
-    }`
-  );
-} else if (operator === '-') {
-  console.log(
-    `${firstNumber} - ${secondNumber} = ${
-      parseInt(firstNumber) - parseInt(secondNumber)
-    }`
-  );
-} else if (operator === '*') {
-  console.log(
-    `${firstNumber} * ${secondNumber} = ${
-      parseInt(firstNumber) * parseInt(secondNumber)
-    }`
-  );
-} else if (operator === '/') {
-  console.log(
-    `${firstNumber} / ${secondNumber} = ${
-      parseInt(firstNumber) / parseInt(secondNumber)
-    }`
-  );
-}
+operator === '+'
+  ? console.log(
+      `${firstNumber} + ${secondNumber} = ${
+        parseInt(firstNumber) + parseInt(secondNumber)
+      }`
+    )
+  : operator === '-'
+  ? console.log(
+      `${firstNumber} - ${secondNumber} = ${
+        parseInt(firstNumber) - parseInt(secondNumber)
+      }`
+    )
+  : operator === '*'
+  ? console.log(
+      `${firstNumber} * ${secondNumber} = ${
+        parseInt(firstNumber) * parseInt(secondNumber)
+      }`
+    )
+  : operator === '/' ??
+    console.log(
+      `${firstNumber} / ${secondNumber} = ${
+        parseInt(firstNumber) / parseInt(secondNumber)
+      }`
+    );
